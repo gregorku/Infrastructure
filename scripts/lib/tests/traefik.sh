ff10aa2acadee2212a6e1256cce030fe01dfd501
Gregor <gregorku@atlas.cz>
2026-07-13 20:58:46 +0200

    test

--- scripts/lib/tests/traefik.sh
+++ scripts/lib/tests/traefik.sh
@@ -1,31 +1,30 @@
+###############################################################################
+#
+# Infrastructure Project
+#
+# File:
+#   scripts/lib/tests/traefik.sh
+#
+# Description:
+#   Traefik validation tests.
+#
+###############################################################################
+
 test_traefik()
 {
     print_section "Traefik"
 
+    #
+    # Container
+    #
     docker_container_running "${TRAEFIK_SERVICE}" \
         || fail "Traefik container is not running."
 
-    docker_exec "${TRAEFIK_SERVICE}" \
-        ss -tln \
-        | grep -q ":8443" \
-        || fail "Admin entrypoint missing."
+    ok "Traefik container OK."
 
-    ok "Admin entrypoint OK."
-
-    docker_exec "${TRAEFIK_SERVICE}" \
-        ss -tln \
-        | grep -q ":443" \
-        || fail "HTTPS entrypoint missing."
-
-    ok "HTTPS entrypoint OK."
-
-    docker_exec "${TRAEFIK_SERVICE}" \
-        ss -tln \
-        | grep -q ":80" \
-        || fail "HTTP entrypoint missing."
-
-    ok "HTTP entrypoint OK."
-
+    #
+    # Dashboard must be protected by BasicAuth.
+    #
     docker_exec "${TRAEFIK_SERVICE}" \
         wget \
             --no-check-certificate \
@@ -34,7 +33,7 @@ test_traefik()
             https://127.0.0.1:8443/dashboard/ \
             2>&1 \
         | grep -q "401 Unauthorized" \
-        || fail "Dashboard authentication missing."
+        || fail "Dashboard authentication is not enabled."
 
     ok "Dashboard authentication OK."
 }
