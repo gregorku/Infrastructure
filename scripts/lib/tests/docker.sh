975ce40a940d1f1fc583e4e09f3d00b2673f2d32
Gregor <gregorku@atlas.cz>
2026-07-13 21:16:06 +0200

    test

--- scripts/lib/tests/docker.sh
+++ scripts/lib/tests/docker.sh
@@ -1,5 +1,27 @@
 ###############################################################################
 #
+# Infrastructure Project
+#
+# File:
+#   scripts/lib/tests/docker.sh
+#
+# Description:
+#   Docker validation tests.
+#
+###############################################################################
+
+test_docker()
+{
+    print_section "Docker"
+
+    docker info >/dev/null 2>&1 \
+        || fail "Docker daemon is not available."
+
+    ok "Docker daemon OK."
+}
+
+###############################################################################
+#
 # Get HTTP response headers from inside a container.
 #
 # Usage:
