a94d23a68348546f1f936bc7edab75750f63fa2d
Gregor <gregorku@atlas.cz>
2026-07-16 19:34:33 +0200

    scripts/shellcheck.sh

--- scripts/lib/tests/shellcheck.sh
+++ scripts/lib/tests/shellcheck.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+###############################################################################
+#
+# Infrastructure Project
+#
+# File:
+#   scripts/shellcheck.sh
+#
+# Description:
+#   Run ShellCheck on all project shell scripts.
+#
+###############################################################################
+
+set -euo pipefail
+
+echo "Running ShellCheck..."
+
 find scripts -name "*.sh" -print0 |
 xargs -0 shellcheck \
     -x \
-    -e SC1091
+    -e SC1091 \
+    -e SC2034
+
+echo
+echo "[ OK ] ShellCheck completed."
