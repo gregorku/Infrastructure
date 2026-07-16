80f17912d7fda85f42550ac4d5464cc3983b9fe5
gregorku <gregorku@atlas.cz>
2026-07-16 10:24:58 +0200

    scripts/lib/env/writer.sh

--- scripts/lib/env/writer.sh
+++ scripts/lib/env/writer.sh
@@ -8,84 +8,15 @@
 #   scripts/lib/env/writer.sh
 #
 # Description:
-#   Write environment configuration files.
+#   Environment file writer.
 #
 # Responsibilities:
-#   - Create new .env
-#   - Preserve comments
-#   - Preserve blank lines
-#   - Write variables in .env.example order
+#   - Backup current .env
+#   - Create temporary .env
+#   - Write synchronized .env
+#   - Replace original .env
 #
 ###############################################################################
-
-###############################################################################
-# Start new environment file
-###############################################################################
-
-env_writer_begin()
-{
-    : > "${ENV_TMP_FILE}"
-}
-
-###############################################################################
-# Write line
-#
-# Arguments:
-#   $1 - Line
-#
-###############################################################################
-
-env_writer_line()
-{
-    printf '%s\n' "$1" >> "${ENV_TMP_FILE}"
-}
-
-###############################################################################
-# Write empty line
-###############################################################################
-
-env_writer_blank()
-{
-    printf '\n' >> "${ENV_TMP_FILE}"
-}
-
-###############################################################################
-# Write comment
-#
-# Arguments:
-#   $1 - Comment
-#
-###############################################################################
-
-env_writer_comment()
-{
-    printf '%s\n' "$1" >> "${ENV_TMP_FILE}"
-}
-
-###############################################################################
-# Write variable
-#
-# Arguments:
-#   $1 - Variable
-#   $2 - Value
-#
-###############################################################################
-
-env_writer_variable()
-{
-    printf '%s=%s\n' "$1" "$2" >> "${ENV_TMP_FILE}"
-}
-
-###############################################################################
-# Finish writing
-###############################################################################
-
-env_writer_finish()
-{
-    mv "${ENV_TMP_FILE}" "${ENV_FILE}"
-
-    ok ".env written."
-}
 
 ###############################################################################
 # Backup environment
@@ -102,4 +33,21 @@ env_backup()
     cp "${ENV_FILE}" "${backup}"
 
     ok "Backup created: $(basename "${backup}")"
+}
+
+###############################################################################
+# Write synchronized environment
+#
+# Arguments:
+#   stdin - Complete .env content
+#
+###############################################################################
+
+env_write_file()
+{
+    cat > "${ENV_TMP_FILE}"
+
+    mv "${ENV_TMP_FILE}" "${ENV_FILE}"
+
+    ok ".env synchronized."
 }
